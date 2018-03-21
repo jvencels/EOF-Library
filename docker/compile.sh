753c06dce75681459e1c16c182af9f20e1068046
jvencels <juris.vencels@gmail.com>
2018-03-21 13:20:04 +0200

    compile.sh added options for choosing OF version

--- docker/compile.sh
+++ docker/compile.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
 
-source ~/.bashrc
 source /opt/openfoam5/etc/bashrc
+source ~/.bashrc
 
-cd $HOME/EOF-Library/libs/commSplit
-cp 5.0-dev/* .
-wmake
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
 
-cd ../coupleElmer
-wmake
+EOFdir='..'            # EOF-Library folder
+SOLVERS='mhdInterFoam' # default solvers for testing
 
-if [ "$#" -eq 0 ]; then
-    echo "Solver name not provided, using the default one.."
-    export SOLVERS="mhdInterFoam"
+while getopts "f:v:s" flag; do
+  case "$flag" in
+    f  ) EOFdir=$OPTARG ;;
+    v  ) OFvers=$OPTARG ;;
+    s  ) SOLVERS=${OPTARG} ;;
+    \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    *  ) echo "Unexpected option ${flag}. Valid options are -f, -v, -s" && exit 1;;
+  esac
+done
+
+validOFvers=("2.4.0" "3.0.1" "4.1" "5.0-stable" "5.0-dev")
+
+cd $EOFdir/libs/commSplit
+echo | pwd
+
+if containsElement "$OFvers" "${validOFvers[@]}"; then
+  echo "Configuring EOF-Library for OpenFOAM version: $OFvers"
+  cp $OFvers/* .
+  wclean && wmake
+elif [ "$OFvers" == "dev" ]; then
+  echo "Configuring EOF-Library for OpenFOAM version: dev"
 else
-    export SOLVERS=$@
+	echo "ERROR: OpenFOAM version $OFvers not supported. Valid versions are:"
+    printf "%s\n" "${validOFvers[@]}"
+    printf "dev\n"
+    exit 1
 fi
+
+echo "Compiling coupler.."
+cd ../coupleElmer
+wclean && wmake
 
 for i in $SOLVERS
 do
-    cd ../"$i"
-    wmake
+  cd ../"$i"
+  wclean && wmake
 done
