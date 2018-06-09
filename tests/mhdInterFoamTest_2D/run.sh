5d0b68ba4e0d7026538a347c3003e1da6d4909d3
jvencels <juris.vencels@gmail.com>
2018-06-09 15:31:01 +0300

    Printing log for travis runs

--- tests/mhdInterFoamTest_2D/run.sh
+++ tests/mhdInterFoamTest_2D/run.sh
@@ -3,8 +3,9 @@
 source /opt/openfoam5/etc/bashrc
 export LD_LIBRARY_PATH=$FOAM_USER_LIBBIN:$LD_LIBRARY_PATH
 
-ElmerGrid 2 2 mesh_Elmer -metis 2 -nooverwrite
-setFields
-decomposePar -force
+ElmerGrid 2 2 mesh_Elmer -metis 2 -nooverwrite &> logElmerGrid.out
+setFields &> logSetFields.out
+decomposePar -force &> logDecomposePar.out
 
-mpirun --allow-run-as-root -n 2 mhdInterFoam -parallel : -n 2 ElmerSolver_mpi
+mpirun --allow-run-as-root -n 2 mhdInterFoam -parallel : -n 2 ElmerSolver_mpi &> logMhdInterFoam.out
+tail logMhdInterFoam.out
